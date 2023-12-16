9c3aee03812e8bdee1a88406341cc67b86c4ec40
Lihue Maidana Avaca <lihuevida@gmail.com>
2023-12-15 22:55:13 -0300

    a ver ahora

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,9 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader',
+            loader: 'url-loader',
           options: {
+            limit: 8192,
             presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
