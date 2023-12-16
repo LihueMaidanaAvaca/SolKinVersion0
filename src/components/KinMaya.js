48cecaf238fed584f16d2fac30e8862f3e5b2e2d
Lihue Maidana Avaca <lihuevida@gmail.com>
2023-12-16 15:51:10 -0300

    babel

--- src/components/KinMaya.js
+++ src/components/KinMaya.js
@@ -4,8 +4,8 @@ import { Box } from '@mui/material';
 const KinMaya = ({ kinMaya }) => {
     return(
         <Box display="flex" alignItems="center" justifyContent="center">
-            <img src={kinMaya.imgTone} alt="Sello Maya" style={{ width: '100px', height: 'auto' }} />
-            <img src={kinMaya.imgSeal} alt="Sello Maya" style={{ width: '200px', height: 'auto' }} />
+            <img src={kinMaya.imgTone} alt="Tono Maya" style={{ width: '100px', height: 'auto' }} />
+            <img src={kinMaya.imgSeal} alt="Sello Maya" style={{ width: '300px', height: 'auto' }} />
         </Box>
     )
 }
